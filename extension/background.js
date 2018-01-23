d51f2ed07873bc265703e36cb2b62c5a437cd875
Domingo Hui <domingo.hui@datadoghq.com>
2018-01-23 14:24:46 -0500

    Add keyboard shortcut

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,27 @@
 (function() {
-  chrome.tabs.onUpdated.addListener(function(id, change_info, tab) {
+  chrome.commands.onCommand.addListener((command) => {
     'use strict';
-    let url = (change_info["url"])? change_info["url"] : "" ;
-    let match_result = url.match(/http(s)?:\/\/s3\.console\.aws\.amazon\.com\/s3\/buckets\//);
-    if (match_result){
-      let s3_path_start_index = match_result["index"] + match_result[0].length;
-      let s3_path_end_index = url.lastIndexOf("/");
-      let path = "s3://"+url.substring(s3_path_start_index, s3_path_end_index);
+    if (command == 'copy') {
+      // Get the current active tab
+      chrome.tabs.query({
+        active: true,
+        currentWindow: true
+      }, (tabs) => {
+        let url = tabs[0].url;
+        let match_result = url.match(/http(s)?:\/\/s3\.console\.aws\.amazon\.com\/s3\/buckets\//);
+        if (match_result){
+          let s3_path_start_index = match_result["index"] + match_result[0].length;
+          let s3_path_end_index = url.lastIndexOf("/");
+          let path = "s3://"+url.substring(s3_path_start_index, s3_path_end_index)+"/";
 
-      let area = document.createElement("textarea");
-      document.body.appendChild(area);
-      area.value = path;
-      area.select();
-      document.execCommand('copy');
-      document.body.removeChild(area)
+          let area = document.createElement("textarea");
+          document.body.appendChild(area);
+          area.value = path;
+          area.select();
+          document.execCommand('copy');
+          document.body.removeChild(area)
+        }
+      });
     }
   });
 })();
